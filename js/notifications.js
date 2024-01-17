7fa310fcb512631dee5681ed0e286260a687ae07
HunigoleSan <soy.hugo.pe@gmail.com>
2024-01-17 10:20:39 -0500

    imagen resultado

--- js/notifications.js
+++ js/notifications.js
@@ -2,60 +2,63 @@ const notificationsHtml = document.getElementById("notifications")
 
 const countHtml = document.getElementById("countNoti").value
 
-let user_notification = [{
+let user_notification = [
+    {
 
-    name: 'Mark Webber',
-    avatar: './assets/images/avatar-mark-webber.webp',
-    reaction: 'reacted to your recent post',
-    postTitle: 'My first tournament today!',
-    notTime: '1m ago',
-    active: false
-}, {
-    name: 'Angela',
-    avatar: './assets/images/avatar-angela-gray.webp',
-    reaction: 'Gray followed you',
-    notTime: '5m ago',
-    active: false
-}, {
-    name: 'Jacob Thompson ',
-    avatar: './assets/images/avatar-jacob-thompson.webp',
-    reaction: 'has joined your group',
-    postTitle: 'Cheese Club',
-    notTime: ' 1 day ago',
-    active: false
-}, {
-    name: 'Rizky Hasanuddin',
-    avatar: './assets/images/avatar-rizky-hasanuddin.webp',
-    reaction: 'sent you a private message',
-    message: 'Hello, thanks for setting up the Chess Club. Ive been a member for a few weeks now and Im already having lots of fun and improving my game.',
-    notTime: '5 days ago',
-    active: false
-}, {
-    name: 'Kimberly Smith',
-    avatar: './assets/images/avatar-kimberly-smith.webp',
-    reaction: 'commented on your picture',
-    commentPicture: './assets/images/image-chess.webp',
-    notTime: '1 week ago',
-    active: false
-}, {
-    name: 'Nathan Peterson',
-    avatar: './assets/images/avatar-nathan-peterson.webp',
-    reaction: 'reacted to your recent post',
-    postTitle: '5 end-game strategies to increase your win rate ',
-    notTime: '2 weeks ago',
-    active: false
-}, {
-    name: 'Anna Kim',
-    avatar: './assets/images/avatar-anna-kim.webp',
-    reaction: 'left the group',
-    postTitle: 'Cheese',
-    notTime: '2 weeks ago',
-    active: false
-}
+        name: 'Mark Webber',
+        avatar: './assets/images/avatar-mark-webber.webp',
+        reaction: 'reacted to your recent post',
+        postTitle: 'My first tournament today!',
+        notTime: '1m ago',
+        active: false
+    }, {
+        name: 'Angela',
+        avatar: './assets/images/avatar-angela-gray.webp',
+        reaction: 'Gray followed you',
+        notTime: '5m ago',
+        active: false
+    }, {
+        name: 'Jacob Thompson ',
+        avatar: './assets/images/avatar-jacob-thompson.webp',
+        reaction: 'has joined your group',
+        postTitle: 'Cheese Club',
+        notTime: ' 1 day ago',
+        active: false
+    }, {
+        name: 'Rizky Hasanuddin',
+        avatar: './assets/images/avatar-rizky-hasanuddin.webp',
+        reaction: 'sent you a private message',
+        message: 'Hello, thanks for setting up the Chess Club. Ive been a member for a few weeks now and Im already having lots of fun and improving my game.',
+        notTime: '5 days ago',
+        active: false
+    }, {
+        name: 'Kimberly Smith',
+        avatar: './assets/images/avatar-kimberly-smith.webp',
+        reaction: 'commented on your picture',
+        commentPicture: './assets/images/image-chess.webp',
+        notTime: '1 week ago',
+        active: false
+    }, {
+        name: 'Nathan Peterson',
+        avatar: './assets/images/avatar-nathan-peterson.webp',
+        reaction: 'reacted to your recent post',
+        postTitle: '5 end-game strategies to increase your win rate ',
+        notTime: '2 weeks ago',
+        active: false
+    }, {
+        name: 'Anna Kim',
+        avatar: './assets/images/avatar-anna-kim.webp',
+        reaction: 'left the group',
+        postTitle: 'Cheese',
+        notTime: '2 weeks ago',
+        active: false
+    }
 ];
+
 let countActive = 0
 
 /* Imprimir Usuarios con notificaciones */
+
 for (let i = 0; i < user_notification.length; i++) {
     let nombre = user_notification[i].name
     let avatar = user_notification[i].avatar
@@ -80,79 +83,80 @@ for (let i = 0; i < user_notification.length; i++) {
     let information_user = `<div class="information">
                                 <div class="info">
                                     <p id="span" class="content_info"> `
-                                    
     let carHtml = ` `
 
-    if(avatar){
+    if (avatar) {
         carHtml += container_user + spanAvatar
     }
     if (nombre) {
         carHtml += information_user + spanNombre
     }
-    if (reaction){
+    if (reaction) {
         carHtml += spanReaction
     }
-    if (postTitle){
+    if (postTitle) {
         carHtml += spanPostTitle
     }
-    if (commetPicture){
+    if (commetPicture) {
         carHtml += spanCommentPicture
     }
     if (active == false) {
-        carHtml += spanActive+`</p>`
-        countActive +=1
+        carHtml += spanActive + `</p>`
+        countActive += 1
     }
-    else{
-        carHtml += spanActive +`</p>`
+    else {
+        carHtml += spanActive + `</p>`
     }
-    
-    if(notTime){
+    if (notTime) {
         carHtml += `<p class="notification_time">
                         ${notTime}
                     </p>`
-                    
+
     }
-    if (messageFriends){
-        carHtml += spanMessage +`</div>
+    if (messageFriends) {
+        carHtml += spanMessage + `</div>
         </div>
         </div>`
     }
-    /* enviar la cantidad a la vista*/
+    /* imprimir usuarios */
     notificationsHtml.innerHTML += carHtml
 }
 
 /* evento click a cada elemento hijo de notificaciones */
 const arrayNotificationsHtml = document.querySelectorAll('.container_user')
+
 const markAllHtml = document.getElementById('markAllNotifications')
 
 document.getElementById("countNoti").innerHTML = ' ' + countActive;
+
 arrayNotificationsHtml.forEach(function callback(currentValue, index, array) {
     /* console.log(array.classList) */
     currentValue.addEventListener('click', e => {
         /* console.log(currentValue.children) */
         var identifiedUser = currentValue.children[1].children[0].children[0].children[0].textContent
         /* var changeActive = currentValue.children[1].children[0].children[0].children[3] */
+        console.log(identifiedUser)
         var changeActive = currentValue.children[1].children[0].children[0].children
-        
+
         for (let i = 0; i < user_notification.length; i++) {
             let nombre = user_notification[i].name
 
             if (identifiedUser == user_notification[i].name) {
                 console.log("Usuario seleccionado")
                 if (user_notification[i].active == false) {
-                    
+
                     user_notification[i].active = true;
                     /* console.log('nombre:',nombre)
                     console.log('active:',user_notification[i].active) */
                     currentValue.classList.remove('show_bg_noti_active')
                     currentValue.classList.add('hidden_bg_noti_not_active')
                     for (let i = 0; i < changeActive.length; i++) {
-                        if(changeActive[i].classList == 'publication'){
+                        if (changeActive[i].classList == 'publication') {
                             changeActive[i].classList.remove('publication')
                             changeActive[i].classList.add('publication_read')
                         }
-                        if (changeActive[i].classList == 'active'){
-                            changeActive[i].classList.remove('active')   
+                        if (changeActive[i].classList == 'active') {
+                            changeActive[i].classList.remove('active')
                         }
                     }
                     countActive -= 1
@@ -166,7 +170,6 @@ arrayNotificationsHtml.forEach(function callback(currentValue, index, array) {
         }
         currentValue.classList.remove('show_bg_noti_active')
         currentValue.classList.add('hidden_bg_noti_not_active')
-        
     })
 
 })
@@ -179,22 +182,22 @@ markAllHtml.addEventListener('click', e => {
             if (user_notification[i].active == false) {
                 user_notification[i].active = true;
                 countActive -= 1
-                /* console.log("Usuario: ", nombre)
-                console.log("Active:", user_notification[i].active) */
+                console.log("Usuario: ", nombre)
+                console.log("Active:", user_notification[i].active)
             } else {
-                /* console.log("No se selecciono a este usuario") */
+                console.log("No se selecciono a este usuario")
             }
             for (let i = 0; i < changeActive.length; i++) {
-                if(changeActive[i].classList == 'publication'){
+                if (changeActive[i].classList == 'publication') {
                     changeActive[i].classList.remove('publication')
                     changeActive[i].classList.add('publication_read')
                 }
-                if (changeActive[i].classList == 'active'){
+                if (changeActive[i].classList == 'active') {
                     changeActive[i].classList.remove('active')
                 }
             }
             currentValue.classList.remove('show_bg_noti_active')
-            currentValue.classList.add('hidden_bg_noti_not_active')  
+            currentValue.classList.add('hidden_bg_noti_not_active')
         }
         document.getElementById("countNoti").innerHTML = ' ' + countActive;
     })
